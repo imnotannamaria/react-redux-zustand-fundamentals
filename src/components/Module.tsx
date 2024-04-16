0b715c5bf01d8f0a69f296ba2f0f546ae10f7801
Anna Maria <aannamariabr@outlook.com>
2024-04-16 16:51:28 -0300

    Migration - Redux to Zustand

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,9 +1,8 @@
 import * as Collapsible from "@radix-ui/react-collapsible";
-
 import { ChevronDown } from "lucide-react";
+
 import { Lesson } from "./Lesson";
-import { useAppDispatch, useAppSelector } from "../store";
-import { play } from "../store/slices/player";
+import { useStore } from "../zustand-store";
 
 interface ModuleProps {
   moduleIndex: number;
@@ -12,19 +11,16 @@ interface ModuleProps {
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
-  const dispatch = useAppDispatch();
-
-  const { currentModuleIndex, currentLesssonIndex } = useAppSelector(
-    (state) => {
-      const { currentModuleIndex, currentLesssonIndex } = state.player;
-
-      return { currentModuleIndex, currentLesssonIndex };
+  const { currentLessonIndex, currentModuleIndex, play, lessons } = useStore(
+    (store) => {
+      return {
+        lessons: store.course?.modules[moduleIndex].lessons,
+        currentLessonIndex: store.currentLessonIndex,
+        currentModuleIndex: store.currentModuleIndex,
+        play: store.play,
+      };
     }
   );
-
-  const lessons = useAppSelector((state) => {
-    return state.player.course?.modules[moduleIndex].lessons;
-  });
 
   return (
     <Collapsible.Root className="group" defaultOpen={moduleIndex === 0}>
@@ -44,18 +40,21 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
           {lessons &&
-            lessons.map((lesson, lessonIndex) => (
-              <Lesson
-                key={lesson.id}
-                title={lesson.title}
-                duration={lesson.duration}
-                isCurrent={
-                  currentModuleIndex === moduleIndex &&
-                  currentLesssonIndex === lessonIndex
-                }
-                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
-              />
-            ))}
+            lessons.map((lesson, lessonIndex) => {
+              const isCurrent =
+                currentModuleIndex === moduleIndex &&
+                currentLessonIndex === lessonIndex;
+
+              return (
+                <Lesson
+                  key={lesson.id}
+                  title={lesson.title}
+                  duration={lesson.duration}
+                  isCurrent={isCurrent}
+                  onPlay={() => play([moduleIndex, lessonIndex])}
+                />
+              );
+            })}
         </nav>
       </Collapsible.Content>
     </Collapsible.Root>
--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -1,8 +1,7 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { useAppSelector } from "..";
+import { PayloadAction, createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { api } from "../../lib/axios";
 
-interface Course  {
+interface Course {
   id: number
   modules: Array<{
     id: number
@@ -11,72 +10,68 @@ interface Course  {
       id: string
       title: string
       duration: string
-    }>;
-  }>;
+    }>
+  }>
 }
 
 export interface PlayerState {
-  course: Course | null
-  currentModuleIndex: number
-  currentLesssonIndex: number
+  course: Course | null;
+  currentModuleIndex: number;
+  currentLessonIndex: number;
+  isLoading: boolean;
 }
 
 const initialState: PlayerState = {
   course: null,
   currentModuleIndex: 0,
-  currentLesssonIndex: 0,
+  currentLessonIndex: 0,
+  isLoading: false,
 }
 
 export const loadCourse = createAsyncThunk(
   'player/load',
   async () => {
-    const response = await api.get("/courses/1")
+    const response = await api.get('/courses/1')
+
     return response.data
   }
 )
 
 export const playerSlice = createSlice({
-  name: 'player',
+  name: "player",
   initialState,
   reducers: {
     play: (state, action: PayloadAction<[number, number]>) => {
       state.currentModuleIndex = action.payload[0]
-      state.currentLesssonIndex = action.payload[1]
+      state.currentLessonIndex = action.payload[1]
     },
     next: (state) => {
-      const nextLessonIndex = state.currentLesssonIndex + 1
-      const nextLesson = state.course?.modules[state.currentModuleIndex].lessons[nextLessonIndex]
-      
+      const nextLessonIndex = state.currentLessonIndex + 1;
+      const nextLesson = state.course?.modules[state.currentModuleIndex].lessons[nextLessonIndex];
+
       if (nextLesson) {
-        state.currentLesssonIndex = nextLessonIndex
-      }
-      else {
-        const nextModuleIndex = state.currentModuleIndex + 1
-        const nextModule = state.course?.modules[nextModuleIndex]
-        
+        state.currentLessonIndex = nextLessonIndex;
+      } else {
+        const nextModuleIndex = state.currentModuleIndex + 1;
+        const nextModule = state.course?.modules[nextModuleIndex];
+
         if (nextModule) {
-          state.currentModuleIndex = nextModuleIndex
-          state.currentLesssonIndex = 0
+          state.currentModuleIndex = nextModuleIndex;
+          state.currentLessonIndex = 0;
         }
       }
     }
   },
   extraReducers(builder) {
+    builder.addCase(loadCourse.pending, (state) => {
+      state.isLoading = true
+    })
     builder.addCase(loadCourse.fulfilled, (state, action) => {
       state.course = action.payload
+      state.isLoading = false
     })
-  }
+  },
 })
 
-export const player = playerSlice.reducer
-export const { play, next } = playerSlice.actions
-
-export const useCurrentLesson = () => {
-  return useAppSelector((state) => {
-    const { currentModuleIndex, currentLesssonIndex } = state.player;
-    const currentModule = state.player.course?.modules[currentModuleIndex];
-    const currentLesson = currentModule?.lessons[currentLesssonIndex];
-
-    return { currentModule, currentLesson };
-  });
-}
+export const player = playerSlice.reducer;
+export const { play, next } = playerSlice.actions;
